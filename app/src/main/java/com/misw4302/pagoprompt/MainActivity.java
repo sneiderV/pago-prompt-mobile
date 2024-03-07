26967c5c81998cb8ea7a7de8079a62353116a890
Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2024-03-06 22:52:10 -0500

    Set onclick listener on login view

--- app/src/main/java/com/misw4302/pagoprompt/MainActivity.java
+++ app/src/main/java/com/misw4302/pagoprompt/MainActivity.java
@@ -1,6 +1,9 @@
 package com.misw4302.pagoprompt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,15 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button loginButton = findViewById(R.id.btn_login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
