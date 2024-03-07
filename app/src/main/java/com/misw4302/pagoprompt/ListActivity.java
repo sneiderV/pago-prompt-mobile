667b8f5f346d6b9a3a21445a3f49e3557e15bc4e
Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2024-03-07 11:54:44 -0500

    Add postpone view

--- app/src/main/java/com/misw4302/pagoprompt/ListActivity.java
+++ app/src/main/java/com/misw4302/pagoprompt/ListActivity.java
@@ -1,6 +1,10 @@
 package com.misw4302.pagoprompt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,21 @@ public class ListActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Busca el TextView por su ID
+        TextView button1TextView = findViewById(R.id.button1TextView);
+
+        // Agrega un OnClickListener al TextView
+        button1TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crea un Intent para iniciar la actividad PostponeActivity
+                Intent intent = new Intent(ListActivity.this, PostponeActivity.class);
+
+                // Inicia la actividad
+                startActivity(intent);
+            }
         });
     }
 }
