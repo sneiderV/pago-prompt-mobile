6478a679bda2524c5356f8b8557072da9b42031a
Fabian Payan <f.payan@uniandes.edu.co>
2024-03-07 20:02:17 -0500

    Se adiciona redireccionamiento de pay activity a pantalla de redireccion.

--- app/src/main/java/com/misw4302/pagoprompt/PayActivity.java
+++ app/src/main/java/com/misw4302/pagoprompt/PayActivity.java
@@ -1,6 +1,9 @@
 package com.misw4302.pagoprompt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,21 @@ public class PayActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Busca el TextView por su ID
+        TextView button2TextView = findViewById(R.id.button2TextView);
+
+        // Agrega un OnClickListener al TextView
+        button2TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crea un Intent para iniciar la actividad PostponeActivity
+                Intent intent = new Intent(PayActivity.this, RedirectActivity.class);
+
+                // Inicia la actividad
+                startActivity(intent);
+            }
         });
     }
 }
